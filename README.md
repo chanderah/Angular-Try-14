caccf4b1d827181e27fc68c5809dc9edaea72a7f
chanderah <68021568+chanderah@users.noreply.github.com>
2023-08-20 22:41:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-Try-14
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-14-title-demo-ngar6q)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    component: DashboardComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
